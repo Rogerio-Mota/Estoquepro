7c0b6f9415fe3fecfb7052dc75b83d64b6155300
Rogerio <pokey4578@gmail.com>
2026-06-02 09:19:02 -0300

    Add backend healthcheck endpoint

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -17,14 +17,17 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import include, path
+from django.urls import include, path, re_path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+from app.health import HealthcheckView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r"^health/?$", HealthcheckView.as_view(), name="healthcheck"),
     path('api/', include('app.urls')),
 
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
